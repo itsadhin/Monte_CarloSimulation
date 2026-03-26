a1f8a6eb126498cca719c87e01176221f777b8bc
itsadhin <itsadhin@gmail.com>
2026-03-27 00:40:37 +0530

    Added random_walk_2D.py

--- random_walk_2D.py
+++ random_walk_2D.py
@@ -1,10 +1,7 @@
 import numpy as np
 
 if __name__ == "__main__":
-    displacement_x = []
-    displacement_y = []
-    square_disp_x = []
-    square_disp_y = []
+    r_square = []
     trial = 1000
     steps = 1000
     for i in range(trial):
@@ -15,12 +12,9 @@ if __name__ == "__main__":
             n_y = np.random.choice([-1,1])
             initial_x += n_x
             initial_y += n_y
-        displacement_x.append(initial_x)
-        square_disp_x.append(initial_x**2)
-        displacement_y.append(initial_y)
-        square_disp_y.append(initial_y**2)
+        r_square.append(initial_x**2 + initial_y**2)
 
-    avg = np.average(square_disp_x + square_disp_y)
+    avg = np.average(r_square)
     print("Average square displacement in 2D: ", avg) # <r^2> = <x^2+y^2>
 
 
